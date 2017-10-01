046a1c6d49e680710d3513e22471e86af1776db8
ericgan7 <eg2584@nyu.edu>
2017-10-01 13:07:31 -0400

    HW2
    Completed pong game.
    Added visual background, fixed frames.

--- HW2/NYUCodebase/NYUCodebase/main.cpp
+++ HW2/NYUCodebase/NYUCodebase/main.cpp
@@ -31,10 +31,19 @@ struct paddle {
 };
 
 struct ball{
+	ball() {
+		yVelocity = (float) (rand() % 40)/10.f;
+	}
 	float ballVerticies[12] = { -0.1f, 0.1f, -0.1f, -0.1f, 0.1f, 0.1f, 0.1f, -0.1f, -0.1f, -0.1f, 0.1f, 0.1f };
 	float bPosition[3] = { 0.0f, 0.0f, 1.0f };
-	float xVelocity = 0.75f;
-	float yVelocity = 0.0f;
+	float xVelocity = 5.5f;
+	float yVelocity;
+};
+
+struct background {
+	float walls[36] = { -10.0f, 6.0f, -10.0f, 5.9f, 10.0f, 6.0f, 10.0f, 5.9f, 10.0f, 6.0f, -10.0f, 5.9f, //top wall
+		-10.0f, -6.0f, -10.0f, -5.9f, 10.0f, -6.0f, 10.0f, -5.9f, 10.0f, -6.0f, -10.0f, -5.9f,			 //bottom wal
+		-0.05f, 6.0f, -0.05f, -6.0f, 0.05f, 6.0f, 0.05f, -6.0f, -0.05f, -6.0f, 0.05f, 6.0f };			 //divider
 };
 
 void drawPaddle(ShaderProgram& program, Matrix& modelviewMatrix, paddle& info) {
@@ -46,7 +55,6 @@ void drawPaddle(ShaderProgram& program, Matrix& modelviewMatrix, paddle& info) {
 	program.SetModelviewMatrix(modelviewMatrix);
 
 	glDrawArrays(GL_TRIANGLES, 0, 6);
-	glDisableVertexAttribArray(program.positionAttribute);
 }
 void drawBall(ShaderProgram& program, Matrix& modelviewMatrix, ball& info) {
 	glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, info.ballVerticies);
@@ -57,23 +65,31 @@ void drawBall(ShaderProgram& program, Matrix& modelviewMatrix, ball& info) {
 	program.SetModelviewMatrix(modelviewMatrix);
 
 	glDrawArrays(GL_TRIANGLES, 0, 6);
-	glDisableVertexAttribArray(program.positionAttribute);
+}
+void drawBackground(ShaderProgram& program, Matrix& modelviewMatrix, background& back, bool clear) {
+	if (clear) {
+		glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, back.walls);
+		glEnableVertexAttribArray(program.positionAttribute);
+		modelviewMatrix.Identity();
+		program.SetModelviewMatrix(modelviewMatrix);
+		glDrawArrays(GL_TRIANGLES, 0, 18);
+	}
 }
 
 void paddleCollision(paddle& p1, paddle& p2, ball& b, float elapsed) {
 	//walls
-	if (abs(p1.position[1] + p1.yVelocity*elapsed) > 5.5f) {
+	if (fabs(p1.position[1] + p1.yVelocity*elapsed) > 5.4f) {
 		if (p1.yVelocity > 0) {
-			p1.position[1] = 5.5f;
+			p1.position[1] = 5.4f;
 		}
-		else { p1.position[1] = -5.5f; }
+		else { p1.position[1] = -5.4f; }
 		p1.yVelocity = 0.0f;
 	}
-	if (abs(p2.position[1] + p2.yVelocity*elapsed) > 5.5f) {
+	if (fabs(p2.position[1] + p2.yVelocity*elapsed) > 5.4f) {
 		if (p2.yVelocity > 0) {
-			p2.position[1] = 5.5f;
+			p2.position[1] = 5.4f;
 		}
-		else { p2.position[1] = -5.5f; }
+		else { p2.position[1] = -5.4f; }
 		p2.yVelocity = 0.0f;
 	}
 }
@@ -87,7 +103,7 @@ bool boxboxCollision(paddle& p, ball& b) {
 
 void ballCollision(paddle& p1, paddle& p2, ball& b, float elapsed) {
 	//wall
-	if (abs(b.bPosition[1] + b.yVelocity*elapsed) > 6.0f) {
+	if (fabs(b.bPosition[1] + b.yVelocity*elapsed) > 5.9f) {
 		b.yVelocity = b.yVelocity *-1.0f;
 	}
 	//paddle & ball
@@ -103,14 +119,36 @@ void ballCollision(paddle& p1, paddle& p2, ball& b, float elapsed) {
 	}
 }
 
-void setMovement(paddle& p1, paddle& p2, ball& b, float elapsed) {
-	if (elapsed > 1.0f/100.0f) {
+bool setMovement(paddle& p1, paddle& p2, ball& b, float elapsed) {
+	if (elapsed > 1.0f / 60.0f) {
 		p1.position[1] += p1.yVelocity*elapsed;
 		p2.position[1] += p2.yVelocity*elapsed;
 		b.bPosition[0] += b.xVelocity*elapsed;
 		b.bPosition[1] += b.yVelocity*elapsed;
+		return true;
 	}
+	return false;
 }
+
+int checkGoal(ball& b) {
+	if (fabs(b.bPosition[0]) > 10.0f) {
+		if (b.bPosition[0] > 0.0f) {
+			return -1;
+		}
+		return 1;
+	}
+	return 0;
+}
+
+void reset(paddle& p1, paddle& p2, ball& b) {
+	p1.position[1] = 0.0f;
+	p2.position[1] = 0.0f;
+	b.bPosition[0] = 0.0f;
+	b.bPosition[1] = 0.0f;
+	b.xVelocity = 0.0f;
+	b.yVelocity = 0.0f;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -127,13 +165,20 @@ int main(int argc, char *argv[])
 	Matrix projectionMatrix;
 	projectionMatrix.SetOrthoProjection(-10.0f, 10.0f, -6.0f, 6.0f, -1.0f, 1.0f);
 	Matrix modelviewMatrix;
-	
+	const Uint8* keyboard;
+	bool wait = false;
+	srand(SDL_GetTicks()%100);
+
 	//Object info
 	float p1[] = {-9.7f, 0.0f, 1.0f };
 	float p2[] = { 9.7f, 0.0f, 1.0f };
 	paddle playerOne(p1);
 	paddle playerTwo(p2);
 	ball ball;
+	background back;
+	float time;
+	int goal;
+	float direction;
 
 	//time
 	float ticks = 0.0f;
@@ -148,43 +193,60 @@ int main(int argc, char *argv[])
 				done = true;
 			}
 		}
-		ticks = (float)SDL_GetTicks() * 0.01f;
-		elapsed = ticks - lastFrameTicks;
-		lastFrameTicks = ticks;
-
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(program.programID);
 		program.SetModelviewMatrix(modelviewMatrix);
 		program.SetProjectionMatrix(projectionMatrix);
 
-		const Uint8* keyboard = SDL_GetKeyboardState(NULL);
+		goal = checkGoal(ball);
+		if (goal != 0) {
+			reset(playerOne, playerTwo, ball);
+			wait = true;
+			time = (float)SDL_GetTicks();
+			direction = (float)goal;
+		}
+
+		keyboard = SDL_GetKeyboardState(NULL);
 		if (keyboard[SDL_SCANCODE_S]) {
-			playerOne.yVelocity = -0.5f;
+			playerOne.yVelocity = -5.0f;
 		}
 		else if (keyboard[SDL_SCANCODE_W]) {
-			playerOne.yVelocity = 0.5f;
+			playerOne.yVelocity = 5.0f;
 		}
 		else { playerOne.yVelocity = 0.0f; }
 		if (keyboard[SDL_SCANCODE_UP]) {
-			playerTwo.yVelocity = 0.5f;
+			playerTwo.yVelocity = 5.0f;
 		}
 		else if (keyboard[SDL_SCANCODE_DOWN]) {
-			playerTwo.yVelocity = -0.5f;
+			playerTwo.yVelocity = -5.0f;
 		}
 		else { playerTwo.yVelocity = 0.0f; }
 
+		ticks = (float)SDL_GetTicks() * 0.001f;
+		elapsed = ticks - lastFrameTicks;
 
 		paddleCollision(playerOne, playerTwo, ball, elapsed);
 		ballCollision(playerOne, playerTwo, ball, elapsed);
-		setMovement(playerOne, playerTwo, ball, elapsed);
+		if (setMovement(playerOne, playerTwo, ball, elapsed)) {
+			lastFrameTicks = ticks;
+		}
 
 		drawPaddle(program, modelviewMatrix, playerOne);
 		drawPaddle(program, modelviewMatrix, playerTwo);
 		drawBall(program, modelviewMatrix, ball);
+		drawBackground(program, modelviewMatrix, back, !wait);
+		glDisableVertexArrayAttrib;
+
+		if (wait) {
+			if ((float)SDL_GetTicks() > time + 1000.0f) {
+				ball.xVelocity = direction * 4.5f;
+				ball.yVelocity = direction * (float)(rand() % 40) / 10.f;
+				wait = false;
+			}
+		}
 
 		SDL_GL_SwapWindow(displayWindow);
 	}
-
 	SDL_Quit();
 	return 0;
 }
